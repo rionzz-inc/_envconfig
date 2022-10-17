5a766e1bc3169817abb1c22c3d63e58f426d0126
Prakash Mishra <prakash.mishra@intimetec.com>
2022-10-17 21:54:36 +0530

    base commit 5

--- tests/pyenv_tests.py
+++ tests/pyenv_tests.py
@@ -1,20 +1,20 @@
 import unittest
 from pathlib import Path
 
-from src.envutil import load_config, constants as Const, Config
+from src.envutil import Config,EnvConst
 
 
 class PyenvTest(unittest.TestCase):
-	BASE_DIR = Path(__file__).parent.resolve().joinpath('sample_data').__str__()
+	BASE_DIR = Path(__file__).parent.parent.resolve().joinpath('test_data').__str__()
 
 	def setUp(self) -> None:
-		load_config(env_path=self.BASE_DIR, file_type=Const.DOT_ENV_FILE)
+		Config.load(env_path=self.BASE_DIR, file_type=EnvConst.DOT_ENV_FILE)
 
 	def test_load_env(self):
 		self.assertTrue(Config.is_loaded())
 
 	def test_get_config(self):
-		self.assertEqual(Config.get(Const.PYENV_LOAD_STATUS_KEY).as_str(), '1')
-		self.assertEqual(Config.get(Const.PYENV_LOAD_STATUS_KEY).as_int(), 1)
-		self.assertEqual(Config.get(Const.PYENV_LOAD_STATUS_KEY).as_bool(), True)
-		self.assertEqual(Config.get(Const.PYENV_LOAD_STATUS_KEY).as_float(), 1.0)
+		self.assertEqual(Config.get(EnvConst.PYENV_LOAD_STATUS_KEY).as_str(), '1')
+		self.assertEqual(Config.get(EnvConst.PYENV_LOAD_STATUS_KEY).as_int(), 1)
+		self.assertEqual(Config.get(EnvConst.PYENV_LOAD_STATUS_KEY).as_bool(), True)
+		self.assertEqual(Config.get(EnvConst.PYENV_LOAD_STATUS_KEY).as_float(), 1.0)
